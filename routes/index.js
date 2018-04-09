4f0b102b09baff45e45672923d2c9e24171ec5e4
ClintonBaker <c.dillon.baker@gmail.com>
2018-04-08 23:34:22 -0500

    Added user registration

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
-import express from "express";
+import express from 'express';
 const router = express.Router();
+const User = require('../models/user');
 
-router.get("*", (req, res) => {
-  res.render("index");
+router.get('/', (req, res) => {
+  res.render('index');
+});
+
+router.post('/register', (req, res) => {
+  const newUser = new User({ username: req.body.username });
+  User.register(newUser, req.body.password);
 });
 
 export default router;
--- server/index.js
+++ server/index.js
@@ -1,5 +1,27 @@
-import app from "../app";
+import app from '../app';
+import mongoose from 'mongoose';
+import passport from 'passport';
+import LocalStrategy from 'passport-local';
+
+const User = require('../models/user');
+
+//Connect to database
+mongoose.connect('mongodb://thebab:pass1234@ds239009.mlab.com:39009/thebab');
+
+//Configure Passport
+app.use(
+  require('express-session')({
+    secret: 'CoWzJump OvArR MooonZ',
+    resave: false,
+    saveUninitialized: false
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.listen(8080, (req, res) => {
-  console.log("Server spinning up!");
+  console.log('Server spinning up!');
 });
